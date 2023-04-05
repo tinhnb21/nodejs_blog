252e404a7da1c1481c5565d9f25f6f57990962bc
Nguyễn Bá Tình <tinhnb.tech@gmail.com>
2023-04-05 11:27:37 +0700

    TinhNB commit prettier, lint-staged, husky

--- src/app/controllers/NewsController.js
+++ src/app/controllers/NewsController.js
@@ -1,14 +1,13 @@
 class NewsController {
-
     //[GET] /news
-    index(req, res){
+    index(req, res) {
         res.render('news');
-    };
+    }
 
     //[GET] /news/:slug
-    show(req, res){
+    show(req, res) {
         res.send('NEWS DETAIL !!!');
-    };
+    }
 }
 
-module.exports = new NewsController;
+module.exports = new NewsController();
--- src/routes/news.js
+++ src/routes/news.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const newsController = require('../app/controllers/NewsController');
 
-router.use('/:slug', newsController.show);  
-router.use('/', newsController.index);  
+router.use('/:slug', newsController.show);
+router.use('/', newsController.index);
 
 module.exports = router;
