2fb0149d73572ac5658199d27b325fdc7df3c6de
Nguyễn Bá Tình <tinhnb.tech@gmail.com>
2023-04-05 22:03:01 +0700

    TinhNB commit

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,11 +1,14 @@
 const Course = require('../models/Course');
+const { multipleMongooseToObject } = require('../../util/mongoose');
 
 class SiteController {
     //[GET] /
     index(req, res, next) {
         Course.find({})
             .then(courses => {
-                res.render('home', { courses });
+                res.render('home', {
+                    courses: multipleMongooseToObject(courses)
+                });
             })
             .catch(next);
     }
