def7c1a613ce0d414297c65bb319770f31579c2d
Nguyễn Bá Tình <tinhnb.tech@gmail.com>
2023-04-06 15:23:54 +0700

    TinhNB commit

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -20,11 +20,11 @@ class CourseController {
 
     //[POST] /courses/store
     store(req, res, next) {
-        const formData = req.body;
+        const formData = { ...req.body };
         formData.image = `https://img.youtube.com/vi/${req.body.videoId}/sddefault.jpg`;
         const course = new Course(formData);
         course.save()
-            .then(() => res.redirect('/'))
+            .then(() => res.redirect('/me/stored/courses'))
             .catch(error => {
 
             });
@@ -52,7 +52,28 @@ class CourseController {
     }
 
     //[DELETE] /courses/:id
+    // delete(req, res, next) {
+    //     Course.deleteOne({ _id: req.params.id })
+    //         .then(() => res.redirect('back'))
+    //         .catch(next);
+    // }
+
+    //soft delete
     delete(req, res, next) {
+        Course.delete({ _id: req.params.id })
+            .then(() => res.redirect('back'))
+            .catch(next);
+    }
+
+    //[PATCH] /courses/:id/restore
+    restore(req, res, next) {
+        Course.restore({ _id: req.params.id })
+            .then(() => res.redirect('back'))
+            .catch(next);
+    }
+
+    //[DELETE] /courses/:id/force
+    forceDestroy(req, res, next) {
         Course.deleteOne({ _id: req.params.id })
             .then(() => res.redirect('back'))
             .catch(next);
--- src/routes/courses.js
+++ src/routes/courses.js
@@ -8,6 +8,8 @@ router.post('/store', courseController.store);
 router.get('/:id/edit', courseController.edit);
 router.put('/:id', courseController.update);
 router.delete('/:id', courseController.delete);
+router.patch('/:id/restore', courseController.restore);
+router.delete('/:id/force', courseController.forceDestroy);
 router.get('/:slug', courseController.show);
 
 module.exports = router;
