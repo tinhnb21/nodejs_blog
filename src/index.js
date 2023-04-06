bbcccad9b42a52e1dca005f5d6fbdb11d7f46ceb
Nguyễn Bá Tình <tinhnb.tech@gmail.com>
2023-04-06 10:21:20 +0700

    TinhNB commit update-courses

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const { engine } = require('express-handlebars');
+const methodOverride = require('method-override');
 const app = express();
 const port = 3000;
 
@@ -11,16 +12,22 @@ const db = require('./config/db');
 //connect to db
 db.connect();
 
+//path đứng ở trang chủ tương ứng với thư mục public
 app.use(express.static(path.join(__dirname, 'public')));
 
 //nhận req.body
+//body parser
+//submit form
 app.use(
     express.urlencoded({
         extended: true,
     }),
 );
-
+//xmlHttpRequest, fetch, axios, ajax,...
 app.use(express.json());
+
+//method override
+app.use(methodOverride('_method'));
 
 //http logger
 // app.use(morgan('combined'));
@@ -30,6 +37,9 @@ app.engine(
     'hbs',
     engine({
         extname: '.hbs',
+        helpers: {
+            sum: (a, b) => a + b,
+        }
     }),
 );
 
