0d3c40bb1778baf18b8bf84df523f1eef4d26263
Nguyễn Bá Tình <tinhnb.tech@gmail.com>
2023-04-06 15:55:29 +0700

    TinhNB count deleted

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -5,9 +5,10 @@ class MeController {
 
     //[GET] /me/stored/courses
     storedCourses(req, res, next) {
-        Course.find({})
-            .then(courses => res.render('me/stored-courses', {
-                courses: multipleMongooseToObject(courses)
+        Promise.all([Course.find({}), Course.countDocumentsDeleted()])
+            .then(([courses, deletedCount]) => res.render('me/stored-courses', {
+                courses: multipleMongooseToObject(courses),
+                deletedCount,
             }))
             .catch(next);
     }
